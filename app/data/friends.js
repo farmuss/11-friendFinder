16961aba91908043d56d2bdc7605f34b05e55898
farmuss <farhod2307@hotmail.com>
2018-10-25 15:04:48 -0600

    working version

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Widowmaker",
+    photo: "https://cdn.gamerant.com/wp-content/uploads/overwatch-widowmaker-statue.jpg.optimal.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Hanzo",
+    photo: "https://www.technobuffalo.com/wp-content/uploads/2017/07/hanzo-overwatch-470x310@2x.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Ana",
+    photo: "http://static3.uk.businessinsider.com/image/578f7e89dd08957d7e8b4a2e/how-to-master-ana-the-newest-character-in-overwatch.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Reinhardt",
+    photo: "http://www.wallpapermaiden.com/image/2018/01/03/overwatch-reinhardt-animation-games-19275.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Soldier 76",
+    photo: "https://i.ytimg.com/vi/XNUgbti7H8Y/maxresdefault.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Moira",
+    photo: "https://i1.wp.com/www.omnicpost.com/wp-content/uploads/2017/11/moira-screenshot-002-e1510846363253.jpg?resize=1200%2C675",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
